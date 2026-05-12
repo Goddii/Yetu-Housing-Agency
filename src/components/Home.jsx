902019d74e5ee769ca974cf053520d25b1297a62
Goddii <otienogodwin23@gmail.com>
2026-05-12 20:59:28 +0300

    style:add featured section to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,26 +15,57 @@ function Home(){
             </div>
             <div className="home-mid-section">
                 <div className="card-one">
+                    <i className="fas fa-list"></i> 
                     <h3>+200 Listings</h3>
                     <p>Premium property listing site</p>
 
                 </div>
                 <div className="card-one">
+                    <i className="fas fa-users"></i> 
                     <h3>Expert Agents</h3>
                     <p>Proffesional and certified realtors</p>
 
                 </div>
                 <div className="card-one">
+                    <i className="fas fa-chart-line"></i>
                     <h3>+10 yrs Experience</h3>
                     <p>Trusted in the real estate market</p>
                 </div>
                 <div className="card-one">
+                    <i className="fas fa-trophy"></i> 
                     <h3>Award Winning</h3>
                     <p>Recognized excellence in service</p>
                 </div>
 
             </div>
             <div className="home-featured-section">
+                <div className="home-card">
+                    <div className="image-card">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="image-section">
+
+                    </div>
+
+                </div>
+                <div className="home-card">
+                    <div className="image-card">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="image-section">
+
+                    </div>
+
+                </div>
+                <div className="home-card">
+                    <div className="image-card">
+
+                    </div>
+                    <div className="image-section">
+
+                    </div>
+
+                </div>
 
             </div>
             <div className="home-last-section">
