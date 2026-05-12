c7578c53272cda04da780745cafdbc0cc5e08332
daudikombe <daudikombe77@gmail.com>
2026-05-12 19:54:17 +0300

    feat:inserting footer and logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,22 @@
 
 function Footer(){
     return(
+        <>
+        {/*FOOTER*/}
+        <footer className="footer">
+          <div className="footer-container">
 
-        <p>Footer</p>
+            {/*LOGO*/}
+            <div>
+                <h2>LuxeEstates</h2>
+                <p>
+                    Your trusted partner in finding the perfect home.
+                    Excellence in real estate since 2010.
+                </p>
+            </div>
+         </div>  
+        </footer>
+        </>
 
     )
 }
