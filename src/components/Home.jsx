8edd0d68aa8eccc600c463585b27c11ef591bf6f
Goddii <otienogodwin23@gmail.com>
2026-05-12 17:50:22 +0300

    style: add image to mid comtainer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,21 @@ import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
 function Home(){
     return(
         <div className="home-section">
-            <h1>Find Your Dream Home</h1>
-            <p>Your trusted partner in finding the perfect home.</p>
-            <Link to='/properties' className="explore-btn">Explore Properties</Link>
+            <div className="home-inside-container">
+                <h1>Find Your Dream Home</h1>
+                <p>Your trusted partner in finding the perfect home.</p>
+            
+                <Link to='/properties' className="explore-btn">Explore Properties</Link>
+            </div>
+            <div className="home-mid-section">
+
+            </div>
+            <div className="home-featured-section">
+
+            </div>
+            <div className="home-last-section">
+
+            </div>    
         </div>
     )
 
