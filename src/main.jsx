5e10435ce1d8ca8895f6175037f206ffc2da61ca
Goddii <otienogodwin23@gmail.com>
2026-05-13 23:43:37 +0300

    fix: resolve link errors, css, page connections and typos

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
-import App from './App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
