bee0b8900af28d3fc188865dd048fcb5ad91af82
daudikombe <daudikombe77@gmail.com>
2026-05-13 03:20:31 +0300

    feat:hero section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,13 +6,16 @@ import {
 } from "react-icons/fa";
 function Contact() {
     return(
-        <div className="contact-section">
-            <h1>Contact Us</h1>
-            <p>For inquiries, please contact us at:</p>
-            <ul>
-                <li>Email: info@yetuhousingagency.com</li>
-                <li>Phone: +1 (123) 456-7890</li>
-            </ul>
+        <div>
+            {/*HERO SECTION*/}
+            <section className="contact-hero">
+                <h1>Contact Us</h1>
+                <P>
+                    We're here to help you find your dream home
+                </P>
+            </section>
+
+    
         </div>
     )
 }
