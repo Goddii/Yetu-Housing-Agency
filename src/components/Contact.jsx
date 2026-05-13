f7ecaf784b1259c81eb36bae5d03608861a5bcc2
daudikombe <daudikombe77@gmail.com>
2026-05-13 04:06:58 +0300

    feat:rsign in  form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,7 +15,60 @@ function Contact() {
                 </P>
             </section>
 
-    
+            {/*MAIN CONTACT SECTION*/}
+            <section className="contact-container">
+
+                {/*LEFT SIDE FORM*/}
+                <div className="contact-form-box">
+                    <h2>Send us a Message</h2>
+
+                    <form>
+                        <div className="form-row">
+                            <div className="form-group">
+                                <label>Full Name</label>
+
+                                <input
+                                type="text"
+                                placeholder="John Doe"
+                                />
+                            </div>
+
+                            <div>
+                                <label>Email Address</label>
+
+                                <input
+                                type="email"
+                                placeholder="john@example.com"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-row">
+                            <div className="form-group">
+                                <label>Subject</label>
+
+                                <select>
+                                    <option>Genersl Inquiry</option>
+                                    <option>Buy Property</option>
+                                    <option>Sell property</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div>
+                            <label>Message</label>
+                            <textarea
+                            rows="7"
+                            placeholder="Tell us about your real estate needs..."></textarea>
+                        </div>
+
+                        <button className="send-btn">
+                            Send Message
+                        </button>
+                    </form>
+                </div>
+                
+            </section>
         </div>
     )
 }
