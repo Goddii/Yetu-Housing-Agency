e65e8657a4cd6a2e9b85114a4be9eb41e5c6ab49
ilhan-moha <mohamudilhan99@gmail.com>
2026-05-14 17:34:01 +0300

    added db.json

--- src/components/Agent.jsx
+++ src/components/Agent.jsx
@@ -1,54 +1,22 @@
+
+import {useState, useEffect} from "react";
 import  "../App.css";
 
 
 
 function Agent() {
-    const agent = [{
-        id: 1,
-        name: "ilhan ahamed",
-        role: "property manager",
-        description: "A skilled professional with a deep understanding of the real estate market, responsible for managing and overseeing properties, ensuring tenant satisfaction, and maximizing property value.",
-        email: "ilhan.ahamed@example.com",
-        listings: [
-            {
-                title: "real estate",
-                location: "south c",
-                price: "$10,000",
-                image: "/images/photo2.avif"
-             },
-                {
-                title: "sana sana estate",
-                location: "south b",
-                price: "$7,000",
-                image: "/images/photo1.avif"
-                }
-            
-        ]
+    const [agent, setAgent] = useState([]);
 
-    },
-    {
-        id: 2,
-        name: "kevin maina",
-        role: "sales manager",
-        description: "An experienced sales manager with a proven track record of driving revenue growth, leading high-performing sales teams, and developing effective sales strategies to achieve business objectives.",
-        email: "kevin.maina@example.com",
-        listings: [
-            {
-                title: "A-one Acacia",
-                location: "Kilimani",
-                price: "$80,000",
-                image: "/images/photo3.avif"
-            },
-            {
-                title: "Libra Residence",
-                location: "nairobi kilimani",
-                price: "$90,000",
-                image: "/images/photo4.avif"
-            },
-           
-        ]
-    }
-];
+    useEffect(() => {
+         fetch ("http://localhost:3001/agents")
+        .then((response) => response.json())
+        .then((data) => {
+            setAgent(data);
+        })
+        .catch((error) => {
+            console.error("Error fetching agent data:", error);
+        });
+ }, []);
 
     return (
         <div className="agent-container">
@@ -65,7 +33,7 @@ function Agent() {
                             <div key={index} className="listing-card">
                                 <h4>{listing.title}</h4>
                                 <p><strong>Location:</strong> {listing.location}</p>
-                                <p className="price"><strong>Price:</strong> {listing.price}</p>
+                                <p style={{ color: 'blue', fontWeight: 'bold' }}><strong>Price:</strong> {listing.price}</p>
                                 <img src={listing.image} alt={listing.title} className="listing-img" />
                             </div>
                         ))}
