49b0cff4c841a2cb58bb099bf970bcc972c088ce
Goddii <otienogodwin23@gmail.com>
2026-05-21 13:07:44 +0300

    feat: add state to listings

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,23 +1,47 @@
 import { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { db } from '../firebase'
+import { collection, addDoc } from "firebase/firestore";
+import './Listings.css'
+import { type } from "firebase/firestore/pipelines";
 
 export default function Listings() {
-    const [listings, setListings] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [submitted, setSubmitted] = useState(false)
+    const [formData, setFormData] = useState({
+        title: '',
+        location: '',
+        price: '',
+        beds: '',
+        baths: '',
+        sqft: '',
+        type: 'house',
+        description: '',
+        image: '',
+        featured: false
+    })
 
     return (
-        <div>
+        <div className="listings-container">
             <form>
-                <label>Add Title</label>
-                <input />
-                <label>Add Location</label>
-                <input />
-                <label>Add Description</label>
-                <input />
-                <label>Add Beds</label>
-                <input />
-                <label>Add Baths</label>
-                <input />
-                <label>Add Sqft</label>
-                <input />
+                <div className="listings-form">
+                    <label>Add Title</label>
+                    <input />
+                    <label>Add Location</label>
+                    <input />
+                    <label>Add Beds</label>
+                    <input />
+                    <label>Add Baths</label>
+                    <input />
+                    <label>Add Sqft</label>
+                    <input />
+                    <label>Add Description</label>
+                    <textarea />
+                    <div>
+                        <label>Add image</label>
+                        <input />
+                    </div>
+                </div>
 
             </form>
         </div>
