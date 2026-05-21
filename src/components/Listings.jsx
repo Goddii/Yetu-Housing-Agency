4e49924ab69905c9df83928e939f646c899c1f71
Goddii <otienogodwin23@gmail.com>
2026-05-21 13:24:02 +0300

    feat: add async function to send data to db

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -21,12 +21,59 @@ export default function Listings() {
         featured: false
     })
 
+    function handleChange(e) {
+        const {name, value, type, checked} = e.target
+        setFormData({
+            ...formData,
+            [name]: type === 'checkbox' ? checked : value
+        })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+            await addDoc(collection(db, 'properties'),{
+                ...formData,
+                price: Number(formData.price),
+                beds: Number(formData.beds),
+                baths: Number(formData.baths),
+                sqft: Number(formData.sqft),
+            })
+            setSubmitted(true)
+            setLoading(false)
+            setFormData({
+                title: '', location: '', price: '', beds: '', baths: '', sqft: '', type: 'house', description: '', image: '', featured: false
+            })
+            setTimeout(() => Navigate('/properties'), 2000)
+        } catch (err) {
+            console.error('Error adding listings', err)
+            setLoading(false)
+        }
+    }
+
     return (
         <div className="listings-container">
-            <form>
+            <div style={{
+                backgroundColor: 'oklch(60.129% 0.20181 259.183)',
+                padding: '40px',
+                color: 'white'
+            }
+                
+            }>
+                <h1>Add New Listing</h1>
+                <p>Fill in the details to add a new property</p>
+
+            </div>
+            <div>
+            <form onSubmit={handleSubmit}>
                 <div className="listings-form">
-                    <label>Add Title</label>
-                    <input />
+                    <label>Property Title</label>
+                    <input name="title"
+                           value={formData.title}
+                           onChange={handleChange} 
+                    />
                     <label>Add Location</label>
                     <input />
                     <label>Add Beds</label>
@@ -44,6 +91,7 @@ export default function Listings() {
                 </div>
 
             </form>
+            </div>
         </div>
     )
 }
