0eeecfa1fadd4610e7b7c05b9bc4e577722054ee
Goddii <otienogodwin23@gmail.com>
2026-05-26 13:54:20 +0300

    style: add grid layout to agents

--- src/components/Agent.jsx
+++ src/components/Agent.jsx
@@ -39,21 +39,29 @@ function Agent() {
             <div className="agent-container">
             {agent.map((agent) => ( 
                  <div key={agent.id} className="agent-card">
-                    <h3 className="agent-name">{agent.name}</h3>
-                    <p><strong>Role:</strong> {agent.role}</p>
-                    <p><strong>Description:</strong> {agent.description}</p>
-                    <p><strong>Email:</strong> {agent.email}</p>
-                    <p><strong>Phone:</strong> {agent.phone}</p>
-                    <h3 className="listings-title">My Listings</h3>
-                    <div className="listings-container">
-                        {agent.listings && agent.listings.map((listing, index) => (
-                            <div key={index} className="listing-card">
-                                <h4>{listing.title}</h4>
-                                <p><strong>Location:</strong> {listing.location}</p>
-                                <p style={{ color: 'blue', fontWeight: 'bold' }}><strong>Price:</strong> {listing.price}</p>
-                                <img src={listing.image} alt={listing.title} className="listing-img" />
-                            </div>
-                        ))}
+                    <div className="agent-logo">
+                        <h3 className="agent-name" style={{display:'inline-block'}}>{agent.name}</h3>
+                        <p><strong>Role:</strong> {agent.role}</p>
+                        <p><strong>Email:</strong> {agent.email}</p>
+                        <p><strong>Phone:</strong> {agent.phone}</p>
+                    </div>
+                    <div className="agent-details">
+                        
+                        <p><strong>Description:</strong> {agent.description}</p>
+
+                    </div>
+                    <div className="agent-listings">
+                        <h3 className="listings-title">My Listings</h3>
+                        <div className="listings-container">
+                            {agent.listings && agent.listings.map((listing, index) => (
+                                <div key={index} className="listing-card">
+                                    <h4>{listing.title}</h4>
+                                    <p><strong>Location:</strong> {listing.location}</p>
+                                    <p style={{ color: 'blue', fontWeight: 'bold' }}><strong>Price:</strong> {listing.price}</p>
+                                    <img src={listing.image} alt={listing.title} className="listing-img" />
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             ))}
